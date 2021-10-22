a59618f0e043cac77a5b3a01e2ba260a02fd7fa8
Paulo Fazio <faulopazio@gmail.com>
2021-10-22 11:56:50 +0200

    github user search prototyped

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
 let imageIndex = 1
 
-const changeSlide = (index) => {
 
-  imageIndex = index + 1
-  slideHandler(imageIndex)
-}
 
 const moveSlider = (factor) => {
   imageIndex += factor
   slideHandler(imageIndex)
 }
-
+// const changeSlide = (index) => {
+//   imageIndex = index + 1
+//   slideHandler(imageIndex)
+// }
+// for (let i = 0; i < sliderDots.length; i++) {
+//   sliderDots[i].classList.remove('active')
+//   // sliderDots[i].addEventListener('click', () => changeSlide(i))
+// }
 
 
 const slideHandler = (index) => {
   const sliderDots = document.getElementsByClassName('dots')
   const slides = document.getElementsByClassName('slider-images')
   
-  for (let i = 0; i < sliderDots.length; i++) {
-    sliderDots[i].classList.remove('active')
-    sliderDots[i].addEventListener('click', () => changeSlide(i))
+  for (let dot of sliderDots) {
+    dot.classList.remove('active')
   }
-
   for (let image of slides) {
     image.style.display = 'none'
   }
@@ -34,3 +35,26 @@ const slideHandler = (index) => {
 }
 
 slideHandler(imageIndex)
+
+const [form] = document.getElementsByClassName('user-search-form')
+const [userToSearch] = document.getElementsByName('username')
+const [resultsContainer] = document.getElementsByClassName('search-results')
+const url = `https://api.github.com/users/${userToSearch.value}`
+
+const handleRepos = async () => {
+  const response = await fetch(`${url}/repos`)
+  const data = await response.json()
+  console.log('repos: ', data)
+}
+
+const handleSearch = async (e) => {
+  e.preventDefault()  
+  const response = await fetch(url)
+  const {public_repos} = await response.json()
+  if (public_repos > 0) {
+    console.log('greater')
+    handleRepos()
+  }
+}
+
+form.onsubmit = handleSearch
